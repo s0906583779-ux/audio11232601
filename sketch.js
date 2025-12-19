c9259c86d1aa3a64784854799e35269fc4bf46f4
s0906583779-ux <s0906583779@gmail.com>
2025-12-19 18:31:06 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/qCAHJR-zF/model.json';
+let soundModelURL = 'https://s0906583779-ux.github.io/audio11232601/model.json';
 
 
 function preload() {
@@ -50,4 +50,5 @@ function gotResult(error, results) {
   // The results are in an array ordered by confidence.
   // console.log(results[0]);
   label = results[0].label;
+
 }
